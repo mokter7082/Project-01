468778957d4b36618d3322f19dd2d2e5499a0909
mokter7082 <mokter7082@gmail.com>
2023-06-03 15:41:24 +0600

    first commit

---
+++ src/Components/Header/header.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div>
+         <div class="grid grid-rows-3 grid-flow-col gap-4">
+              
+            <div>
+                <h1>Shopping Center</h1>
+            </div>
+            <div>
+                <ul>
+                <li>Products</li>
+                <li>Wishlist</li>
+                <li>My Collection</li>
+                </ul>
+            </div>
+
+             </div>
+    </div>
+  )
+}
